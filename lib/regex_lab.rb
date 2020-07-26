b28bbabc34be84a7c6c4c9175a42e4baf297e1ab
Sierra Wallace <sskwallace@gmail.com>
2020-07-26 05:18:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,9 +17,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan
-
-end
+  if text.scan(/^[A-Z].+[\.!?]/)
+    return true
+  else
+    return false
+  end
 
 def valid_phone_number?(phone)
 
