c7ca51b2fb3ece5375878d514231df66b1ed356a
Sierra Wallace <sskwallace@gmail.com>
2020-07-26 05:15:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,23 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/^[aeiouAEIOU]/).empty?
+    false
+  else
+    true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words = text.split
+  words.grep(/^un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  words = text.split
+  words.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.scan
 
 end
 
