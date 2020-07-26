eac7b8477916610892a2313b0534291c92db0e2e
Sierra Wallace <sskwallace@gmail.com>
2020-07-26 05:22:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]+[\s\S].+[\.!?]/)
+  if text.match(/^[A-Z].+[\.!?]$/)
     return true
   else
     return false
